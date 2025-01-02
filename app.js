56b47e5662094f1a926c80fde92de27d9454a22d
GersonESS <gebhsantos@gmail.com>
2025-01-02 16:41:27 -0300

    a

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 // Importar módulo express
 const express = require('express');
 
-const mysql = require('mysql2');
+// Importar módulo fileupload
+const fileUpload = require('express-fileupload');
 
+// Importar módulo express-handlebars
+const { engine } = require('express-handlebars');
+
+// Importar módulo mysql
+
+const mysql = require('mysql2');
 // App
 const app = express();
+
+// Adicionar fileupload
+app.use(fileUpload());
+
+
+//adicionar Bootstrap
+
+app.use('/Bootstrap', express.static('./node_modules/bootstrap/dist'));
+
+//adicionar css
+
+app.use('/css', express.static('./css'));
+
+// Configuração do Handlebars
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');   
+app.set('views', './views');
+
+//Manipulação de dados  
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 const conexao = mysql.createConnection({
     host: 'localhost', 
@@ -14,17 +44,24 @@ const conexao = mysql.createConnection({
     });
 
 
-conexao.connect(function(err){
-    if(err) return console.log(err);
+conexao.connect(function(erro){
+    if(erro) throw erro;
     console.log('Conectou!');
-    createTable(conexao);
+    //createTable(conexao);
 })
 
-
-
-// Rota de teste
+// Rota principal
 app.get('/', function(req, res){
-    res.end('Hello World!');
+    res.render('formulario');
+});
+
+// Rota de cadastro
+
+app.post('/cadastrar', function(req, res){
+console.log(req.body);
+console.log(req.files.imagem.name);
+req.files.imagem.mv(__dirname+'/imagens/'+ req.files.imagem.name);
+res.end();
 });
 
 // Servidor
